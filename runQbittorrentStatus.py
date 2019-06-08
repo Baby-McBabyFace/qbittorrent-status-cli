c14669dbbcd238bc13d6c2d7343dc503eb905060
Baby-McBabyFace <32679064+Baby-McBabyFace@users.noreply.github.com>
2019-06-08 21:13:52 +0800

    Add files via upload

--- runQbittorrentStatus.py
+++ runQbittorrentStatus.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 from qbittorrent import Client
-from terminaltables import DoubleTable
+from terminaltables import AsciiTable
 
 def speed_value(speed):
     if (speed < 1024):
@@ -13,10 +13,27 @@ def speed_value(speed):
     else:
         return "WTF"
 
+def session_data_value(data_session):
+    if (data_session < 1024):
+        return str(data_session) +" B"
+    elif (data_session < 1024**2):
+        return "{:.2f} KiB".format(data_session/1024)
+    elif (data_session < 1024**3):
+        return "{:.2f} MiB".format(data_session/1024**2)
+    elif (data_session < 1024**4):
+        return "{:.2f} GiB".format(data_session/1024**3)
+    elif (data_session < 1024**5):
+        return "{:.2f} TiB".format(data_session/1024**4)
+    else:
+        return "WTF"
+
 qb = Client("http://localhost:8080/")
 
 downloadSpeed = qb.global_transfer_info["dl_info_speed"]
 uploadSpeed = qb.global_transfer_info["up_info_speed"]
+
+downloadSessionData = qb.global_transfer_info["dl_info_data"]
+uploadSessionData = qb.global_transfer_info["up_info_data"]
 
 table_data = [
     ["qBittorrent-nox"],
@@ -26,9 +43,11 @@ table_data = [
     ["Total number of Downloading torrents: ", len(qb.torrents(filter="downloading"))],
     ["Total number of Paused torrents: ", len(qb.torrents(filter="paused"))],
     ["Download Speed: ", speed_value(downloadSpeed)],
-    ["Upload Speed: ", speed_value(uploadSpeed)]
+    ["Upload Speed: ", speed_value(uploadSpeed)],
+    ["Download Speed: ", session_data_value(downloadSessionData)],
+    ["Upload Speed: ", session_data_value(uploadSessionData)]
     ]
     
-table = DoubleTable(table_data)
+table = AsciiTable(table_data)
 table.justify_columns[1]="right"
 print(table.table)
